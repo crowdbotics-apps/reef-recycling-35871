754c3e87f4a2d8e43c82932b5691afb78a6a5c01
Crowdbotics <team@crowdbotics.com>
2022-06-22 21:00:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "reef_recycling_35871",
+  url: "https://reef_recycling_35871.botics.co",
+  api: "https://reef_recycling_35871.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
